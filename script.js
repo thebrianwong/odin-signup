1de72257f3b1719f5e7f0224db55fb9011fdec8c
Brian Wong <brianw7218@gmail.com>
2022-09-24 16:48:58 -0700

    Add functionality to prevent submission if passwords do not match

--- script.js
+++ script.js
@@ -1,27 +1,35 @@
 const addPasswordsCheck = () => {
     password = document.querySelector("#password");
     passwordConfirm = document.querySelector("#confirm-password");
-    errorMessage = document.querySelector("#error-message");
-    password.addEventListener("blur", checkPasswords.bind(this, password, passwordConfirm, errorMessage));
-    passwordConfirm.addEventListener("blur", checkPasswords.bind(this, password, passwordConfirm, errorMessage));
+    passwordMessage = document.querySelector("#password-message");
+    password.addEventListener("blur", checkPasswords.bind(this, password, passwordConfirm, passwordMessage));
+    passwordConfirm.addEventListener("blur", checkPasswords.bind(this, password, passwordConfirm, passwordMessage));
 }
 
-const checkPasswords = (password, passwordConfirm, errorMessage) => {
+const checkPasswords = (password, passwordConfirm, passwordMessage) => {
     if (passwordConfirm.value !== password.value && passwordConfirm.value !== "" && password.value !== "") {
         password.classList.add("password-error");
         passwordConfirm.classList.add("password-error")
-        errorMessage.style.visibility = "visible";
+        passwordMessage.style.visibility = "visible";
     } else {
         password.classList.remove("password-error");
         passwordConfirm.classList.remove("password-error");
-        errorMessage.style.visibility = "hidden";
+        passwordMessage.style.visibility = "hidden";
+        submitMessage = document.querySelector("#button-message");
+        submitMessage.style.visibility = "hidden";
     }
 }
 
 const addSubmitCheck = () => {
     submitButton = document.querySelector("button");
-    submitButton.addEventListener("click", () => {
-
+    submitMessage = document.querySelector("#button-message");
+    password = document.querySelector("#password");
+    passwordConfirm = document.querySelector("#confirm-password");
+    submitButton.addEventListener("click", (event) => {
+        if (passwordConfirm.value !== password.value) {
+            submitMessage.style.visibility = "visible";
+            event.preventDefault();
+        }
     })
 }
 
