ca301971108b300ec8bde6f01f318c02b50aa329
Brian Wong <brianw7218@gmail.com>
2022-09-24 16:23:56 -0700

    Add functionality to display password error message when appropriate

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
 const addPasswordsCheck = () => {
     password = document.querySelector("#password");
     passwordConfirm = document.querySelector("#confirm-password");
-    password.addEventListener("blur", checkPasswords.bind(this, password, passwordConfirm));
-    passwordConfirm.addEventListener("blur", checkPasswords.bind(this, password, passwordConfirm));
+    errorMessage = document.querySelector("#error-message");
+    password.addEventListener("blur", checkPasswords.bind(this, password, passwordConfirm, errorMessage));
+    passwordConfirm.addEventListener("blur", checkPasswords.bind(this, password, passwordConfirm, errorMessage));
 }
 
-const checkPasswords = (password, passwordConfirm) => {
+const checkPasswords = (password, passwordConfirm, errorMessage) => {
     if (passwordConfirm.value !== password.value && passwordConfirm.value !== "" && password.value !== "") {
         password.classList.add("password-error");
         passwordConfirm.classList.add("password-error")
+        errorMessage.style.visibility = "visible";
     } else {
         password.classList.remove("password-error");
         passwordConfirm.classList.remove("password-error");
+        errorMessage.style.visibility = "hidden";
     }
 }
 
