52a68d755fbcaac5f4f9dde5b9d903870b372c62
Brian Wong <brianw7218@gmail.com>
2022-09-24 18:59:37 -0700

    Improve password match check

--- script.js
+++ script.js
@@ -2,22 +2,60 @@ const addPasswordsCheck = () => {
     password = document.querySelector("#password");
     passwordConfirm = document.querySelector("#confirm-password");
     passwordMessage = document.querySelector("#password-message");
-    password.addEventListener("blur", checkPasswords.bind(this, password, passwordConfirm, passwordMessage));
-    passwordConfirm.addEventListener("blur", checkPasswords.bind(this, password, passwordConfirm, passwordMessage));
+    password.addEventListener("blur", checkPassword.bind(this, password, passwordConfirm, passwordMessage));
+    password.addEventListener("keyup", checkPassword.bind(this, password, passwordConfirm, passwordMessage));
+    passwordConfirm.addEventListener("blur", checkPasswordConfirm.bind(this, password, passwordConfirm, passwordMessage));
+    passwordConfirm.addEventListener("keyup", checkPasswordConfirm.bind(this, password, passwordConfirm, passwordMessage));
 }
 
-const checkPasswords = (password, passwordConfirm, passwordMessage) => {
-    if (passwordConfirm.value !== password.value && passwordConfirm.value !== "" && password.value !== "") {
-        password.classList.add("password-error");
-        passwordConfirm.classList.add("password-error")
-        passwordMessage.style.visibility = "visible";
-    } else {
-        password.classList.remove("password-error");
-        passwordConfirm.classList.remove("password-error");
-        passwordMessage.style.visibility = "hidden";
-        submitMessage = document.querySelector("#button-message");
-        submitMessage.style.visibility = "hidden";
+const checkPassword = (password, passwordConfirm, passwordMessage) => {
+    if (passwordConfirm.value === "" && password.value === "") {
+        resetMessages(password, passwordConfirm, passwordMessage);
+    } else if (passwordConfirm.value !== password.value && passwordConfirm.value !== "") {
+        addErrorMessages(password, passwordConfirm, passwordMessage);
+    } else if (passwordConfirm.value === password.value) {
+        addConfirmationMessages(password, passwordConfirm, passwordMessage);
     }
+}
+
+const checkPasswordConfirm = (password, passwordConfirm, passwordMessage) => {
+    if (passwordConfirm.value === "" && password.value === "") {
+        resetMessages(password, passwordConfirm, passwordMessage);
+    } else if (passwordConfirm.value !== password.value) {
+        addErrorMessages(password, passwordConfirm, passwordMessage);
+    } else if (passwordConfirm.value === password.value) {
+        addConfirmationMessages(password, passwordConfirm, passwordMessage);
+    }
+}
+
+const addErrorMessages = (password, passwordConfirm, passwordMessage) => {
+    password.classList.add("password-error");
+    passwordConfirm.classList.add("password-error")
+    password.classList.remove("password-match");
+    passwordConfirm.classList.remove("password-match")
+    passwordMessage.textContent = "* Passwords do not match!";
+    passwordMessage.style.color = "Red";
+    passwordMessage.style.visibility = "visible";
+}
+
+const addConfirmationMessages = (password, passwordConfirm, passwordMessage) => {
+    password.classList.add("password-match");
+    passwordConfirm.classList.add("password-match")
+    password.classList.remove("password-error");
+    passwordConfirm.classList.remove("password-error")
+    passwordMessage.textContent = "\u2713 Passwords match!";
+    passwordMessage.style.color = "Green";
+    passwordMessage.style.visibility = "visible";
+    submitMessage = document.querySelector("#button-message");
+    submitMessage.style.visibility = "hidden";
+}
+
+const resetMessages = (password, passwordConfirm, passwordMessage) => {
+    password.classList.remove("password-match");
+    passwordConfirm.classList.remove("password-match")
+    password.classList.remove("password-error");
+    passwordConfirm.classList.remove("password-error")
+    passwordMessage.style.visibility = "hidden";
 }
 
 const addSubmitCheck = () => {
